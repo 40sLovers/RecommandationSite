86d9883d6dd4c660c8bab7edc8f2035c125dc4bb
PaulIRL <a@a.co>
2021-11-24 20:25:57 +0200

    added example json response

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,6 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns=[
-    path("hello/",views.helloWorld)
+    path("hello/",views.helloWorld),
+    path("movies/",views.jsonResponseExample)
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+
 # Create your views here.
 
 def helloWorld(request):
     return HttpResponse("helloWorld")
+
+def jsonResponseExample(request):
+    d=dict()
+    d["foo"]="bar"
+    l=["Film1",[d,"Exemplu"],"Film3"]
+    return HttpResponse(JsonResponse(l,safe=False))
